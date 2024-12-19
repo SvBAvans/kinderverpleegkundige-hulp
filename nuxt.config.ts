4d024176a2fbe9ac4011ebeba77fb15990e3554c
Siem van Breda <sgw.vanbreda@student.avans.nl>
2024-12-19 13:37:27 +0100

    Added Bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  css: ["~/node_modules/bootstrap/dist/css/bootstrap.min.css"],
+  plugins: [{ src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" }],
+});
