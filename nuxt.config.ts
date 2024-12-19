e94baa6bcb8846850680cac1ae844aa98df8af80
Siem van Breda <sgw.vanbreda@student.avans.nl>
2024-12-19 13:55:54 +0100

    Add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
   css: ["~/node_modules/bootstrap/dist/css/bootstrap.min.css"],
   plugins: [{ src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" }],
+  modules: ["@nuxt/icon"],
 });
