2e1ced02c8f43bf514d83d38f9fb9661cd50449d
Siem van Breda <sgw.vanbreda@student.avans.nl>
2025-01-07 11:19:05 +0100

    Add login form validation

--- pages/login.vue
+++ pages/login.vue
@@ -1,54 +1,64 @@
 <script setup lang="ts">
+import { object, string } from "zod";
 
-    const user = ref({
-        userName: "",
-        password: ""
-    });
-    
+const schema = toTypedSchema(
+  object({
+    email: string().min(1, { message: "Email is verplicht" }).email({ message: "Ongeldig email" }),
+    password: string().min(1, { message: "Wachtwoord is verplicht" }),
+  })
+);
+
+const { handleSubmit, errors } = useForm({
+  validationSchema: schema,
+});
+
+const { value: email } = useField("email");
+const { value: password } = useField("password");
+
+const onSubmit = handleSubmit((values) => {
+  alert("Submitted");
+  console.log(values);
+});
 </script>
 
 <template>
-    <div class="d-flex vh-100 justify-content-center align-items-center">
-        <form class="login-form was-validated ms-4 me-4 bg-light shadow p-3 rounded" novalidate>
-            <div>
-                <input class="form-control input-lg col-12 border border-secondary" id="email" name="email" type="text" v-model="user.userName" placeholder="Gebruikersnaam" required />
-                <div class="invalid-feedback">
-                    Gebruikersnaam is verplicht
-                </div>
-            </div>
-            <div class="mt-3">
-                <input class="form-control input-lg col-12 border border-secondary" id="password" name="password" type="password" v-model="user.password" placeholder="Wachtwoord" required />
-                <div class="invalid-feedback">
-                    Wachtwoord is verplicht
-                </div>
-            </div>
-            <button class="login-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Login</button>
-        </form>
-    </div>
+  <div class="d-flex vh-100 justify-content-center align-items-center">
+    <form @submit="onSubmit" class="login-form ms-4 me-4 bg-light shadow p-3 rounded">
+      <div>
+        <input class="form-control input-lg col-12 border border-secondary" :class="{ 'is-invalid': errors.email }" name="email" type="email" v-model="email" placeholder="Email" />
+        <div v-if="errors.email" class="invalid-feedback">{{ errors.email }}</div>
+      </div>
+
+      <div class="mt-3">
+        <input class="form-control input-lg col-12 border border-secondary" :class="{ 'is-invalid': errors.password }" name="password" type="password" v-model="password" placeholder="Wachtwoord" />
+        <div v-if="errors.password" class="invalid-feedback">{{ errors.password }}</div>
+      </div>
+      <button class="login-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <style>
-    .login-form {
-        width: 100%;
-        max-width: 400px;
-    }
+.login-form {
+  width: 100%;
+  max-width: 400px;
+}
 
-    @media screen and (min-width: 768px) {
-        .login-form {
-            width: 100%;
-            max-width: 600px;
-            font-size: 25px;
-        }
+@media screen and (min-width: 768px) {
+  .login-form {
+    width: 100%;
+    max-width: 600px;
+    font-size: 25px;
+  }
 
-        input.form-control {
-            font-size: 20px;
-            padding: 15px;
-        }
+  input.form-control {
+    font-size: 20px;
+    padding: 15px;
+  }
 
-        button.login-button {
-            padding: 15px;
-            font-size: 25px;
-        }
-    }
+  button.login-button {
+    padding: 15px;
+    font-size: 25px;
+  }
+}
 </style>
-
