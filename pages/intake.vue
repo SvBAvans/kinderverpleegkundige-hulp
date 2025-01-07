2fa192510591e75dc61b39b46ad5788faa0bbf08
casstaal <casstaal@hotmail.com>
2025-01-07 13:54:39 +0100

    Added validation and post request

--- pages/intake.vue
+++ pages/intake.vue
@@ -1,98 +1,112 @@
 <script setup lang="ts">
-const patient = ref({
-  firstName: "",
-  lastName: "",
-  patientId: "",
-  dateOfBirth: null,
-  diseaseProfile: ""
-});
+    import { toTypedSchema } from "@vee-validate/zod";
+    import { object, string } from "zod";
+    import type { FetchError } from "ofetch";
+    import type { Patient } from "@prisma/client";
 
-const isFormValid = computed(() => {
-  return (
-    patient.value.firstName.trim() !== "" &&
-    patient.value.lastName.trim() !== "" &&
-    patient.value.patientId.trim() !== "" &&
-    patient.value.dateOfBirth !== null &&
-    patient.value.diseaseProfile.trim() !== ""
-  );
-});
+    const errorMessage = ref("");
+    const error = ref(false);
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// onMounted(() => {
-//   'use strict';
+    const schema = toTypedSchema(
+    object({
+        firstName: string().min(1, { message: "Voornaam is verplicht" }),
+        lastName: string().min(1, { message: "Achternaam is verplicht" }),
+        patientId: string().min(1, { message: "PatientID is verplicht" }),
+        dateOfBirth: string({ required_error: "Geboortedatum is verplicht" }).date(),
+        diseaseProfile: string().min(1, { message: "Ziektebeeld is verplicht "})
+    })
+    );
 
-//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//   const forms = document.querySelectorAll('.needs-validation') as NodeListOf<HTMLFormElement>;
+    const { handleSubmit, errors } = useForm({
+    validationSchema: schema,
+    });
 
-//   // Loop over them and prevent submission
-//   Array.from(forms).forEach(form => {
-//     form.addEventListener('submit', event => {
-//       if (!form.checkValidity()) {
-//         event.preventDefault();
-//         event.stopPropagation();
-//       }
+    const { value: firstName } = useField("firstName");
+    const { value: lastName } = useField("lastName");
+    const { value: patientId } = useField("patientId");
+    const { value: dateOfBirth } = useField("dateOfBirth");
+    const { value: diseaseProfile } = useField("diseaseProfile");
 
-//       form.classList.add('was-validated');
-//     }, false);
-//   });
-// });
+    async function createPatient(values: any) {
+        values.dateOfBirth = new Date(values.dateOfBirth)
+        const response = await $fetch<Patient>("/api/patients", { method: "post",
+            body: values  }).catch((e: FetchError) => {
+                errorMessage.value = e.data.message;
+                error.value = true;
+            });
+
+            if (error.value) {
+                return;
+            }
+
+            if (!response) {
+                errorMessage.value = "An error occurred";
+                error.value = true;
+                return;
+            }
+
+            navigateTo(`patients/${response.id}`)  
+    }
+
+    const onSubmit = handleSubmit(async (values) => {
+        createPatient(values);
+    })
 
 </script>
 
 <template>
-    <!-- <div class="ms-2 mt-2 d-flex flex-row">
-        <Icon name="charm:menu-hamburger" size="50px" />
-        <h1 class="nav col-12 justify-content-center mt-2">Intake Patient</h1>
-    </div> -->
-
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
-        <div class="col-md-3 mb-2 mb-md-0">
-            <Icon class="d-inline-flex link-body-emphasis text-decoration-none" name="charm:menu-hamburger" size="50px" />
-        </div>
-        <h1 class="col-32 col-md-auto mb-2 justify-content-center mb-md-0">Intake</h1>
-    </header>
-
-    <hr />
-
-    <form class="was-validated" novalidate>
-        <div class="ms-4 me-4 mt-4">
-            <div class="mt-3">
+    <div class="d-md-flex vh-100 justify-content-center align-items-center">
+        <form @submit="onSubmit" class="intake-form col-sm-12 col-md-8 pe-0  ms-4 me-4 bg-light shadow rounded pt-1 pb-3">
+        <div class="ms-4 me-4">
+            <div class="mt-2">
                 <label for="firstName" class="form-label">Voornaam:</label>
-                <input class="form-control input-lg col-12 border border-secondary" id="firstName" type="text" v-model="patient.firstName" placeholder="Voornaam" required  />
-                <div class="invalid-feedback">
-                    Voornaam is verplicht
-                </div>
+                <input class="form-control input-lg border border-secondary" :class="{ 'is-invalid': errors.firstName }" id="firstName" type="text" v-model="firstName" placeholder="Voornaam" />
+                <div v-if="errors.firstName" class="invalid-feedback">{{ errors.firstName }}</div>
             </div>
             <div class="mt-3">
                 <label for="lastName">Achternaam:</label>
-                <input class="form-control input-lg col-12 border border-secondary" id="lastName" type="text" v-model="patient.lastName" placeholder="Achternaam" required  />
-                <div class="invalid-feedback">
-                    Achternaam is verplicht
-                </div>
+                <input class="form-control input-lg border border-secondary" :class="{ 'is-invalid': errors.lastName }" id="lastName" type="text" v-model="lastName" placeholder="Achternaam" />
+                <div v-if="errors.lastName" class="invalid-feedback">{{ errors.lastName }}</div>
+
             </div>    
             <div class="mt-3">
                 <label for="patientID">Patient-ID:</label>
-                <input class="form-control input-lg col-12 border border-secondary" id="patientID" type="text" v-model="patient.patientId" placeholder="Patient-ID" required  />
-                <div class="invalid-feedback">
-                    Patient ID is verplicht
-                </div>
+                <input class="form-control input-lg border border-secondary" :class="{ 'is-invalid': errors.patientId }" id="patientID" type="text" v-model="patientId" placeholder="Patient-ID" />
+                <div v-if="errors.patientId" class="invalid-feedback">{{ errors.patientId }}</div>
+
             </div>
             <div class="mt-3">
                 <label for="dateOfBirth">Geboortedatum:</label>
-                <input class="form-control input-lg col-12 border border-secondary" id="dateOfBirth" type="date" v-model="patient.dateOfBirth" placeholder="Geboortedatum" required  />
-                <div class="invalid-feedback">
-                    Geboortedatum is verplicht
-                </div>
+                <input class="form-control input-lg border border-secondary" :class="{ 'is-invalid': errors.dateOfBirth }" id="dateOfBirth" type="date" v-model="dateOfBirth" placeholder="Geboortedatum" />
+                <div v-if="errors.dateOfBirth" class="invalid-feedback">{{ errors.dateOfBirth }}</div>
             </div>
             <div class="mt-3">
                 <label>Ziektebeeld:</label>
-                <textarea class="col-12" rows="5" v-model="patient.diseaseProfile" placeholder="Ziektebeeld" required></textarea>
-                <div class="invalid-feedback">
-                    Ziektebeeld is verplicht
-                </div>
+                <textarea class="col-12" rows="5" :class="{ 'is-invalid': errors.diseaseProfile }" v-model="diseaseProfile" placeholder="Ziektebeeld"></textarea>
+                <div v-if="errors.diseaseProfile" class="invalid-feedback">{{ errors.diseaseProfile }}</div>
             </div>
-            <button class="btn btn-secondary btn-lg mt-3 col-12" type="submit" :disabled="!isFormValid">Afronden</button>
+            <button class="intake-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Afronden</button>
         </div>
     </form>
+    </div>
 </template>
+
+
+<style>
+
+    @media screen and (min-width: 768px) {
+        .intake-form {
+            font-size: 25px;
+        }
+
+        input.form-control {
+            font-size: 22px;
+            padding: 15px;
+        }
+
+        button.intake-button {
+            font-size: 25px;
+        }
+    }
+</style>
   
