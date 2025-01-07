34a991946e75997ad2ee695ced4a5bbaca03fa4e
Siem van Breda <sgw.vanbreda@student.avans.nl>
2025-01-07 14:16:51 +0100

    Secret & origin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/icon", "@vee-validate/nuxt", "@sidebase/nuxt-auth"],
 
+  runtimeConfig: {
+    authSecret: "",
+  },
+
   veeValidate: {
     autoImports: true,
     componentNames: {
