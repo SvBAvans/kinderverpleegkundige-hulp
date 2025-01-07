696f3ab519d9e7dd4de0740c1530eb31f3ef02b5
casstaal <casstaal@hotmail.com>
2025-01-07 10:49:20 +0100

    Made login form responsive for tablet screens

--- pages/login.vue
+++ pages/login.vue
@@ -9,7 +9,7 @@
 
 <template>
     <div class="d-flex vh-100 justify-content-center align-items-center">
-        <form class="was-validated ms-4 me-4 bg-light shadow p-3 rounded" style="width: 100%; max-width: 400px;" novalidate>
+        <form class="login-form was-validated ms-4 me-4 bg-light shadow p-3 rounded" novalidate>
             <div>
                 <input class="form-control input-lg col-12 border border-secondary" id="email" name="email" type="text" v-model="user.userName" placeholder="Gebruikersnaam" required />
                 <div class="invalid-feedback">
@@ -22,8 +22,33 @@
                     Wachtwoord is verplicht
                 </div>
             </div>
-            <button class="btn btn-secondary btn-lg mt-3 col-12" type="submit">Login</button>
+            <button class="login-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Login</button>
         </form>
     </div>
 </template>
+
+<style>
+    .login-form {
+        width: 100%;
+        max-width: 400px;
+    }
+
+    @media screen and (min-width: 768px) {
+        .login-form {
+            width: 100%;
+            max-width: 600px;
+            font-size: 25px;
+        }
+
+        input.form-control {
+            font-size: 20px;
+            padding: 15px;
+        }
+
+        button.login-button {
+            padding: 15px;
+            font-size: 25px;
+        }
+    }
+</style>
 
