7e93a0877263d9d15771aa03e30e5e7b045d4afe
Fleur <fleurmaxyara@gmail.com>
2025-01-07 12:03:57 +0100

    it now works - changes to bootstrap plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/node_modules/bootstrap/dist/css/bootstrap.min.css"],
-  plugins: [{ src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" }],
+  plugins: [{ src: "~/plugins/bootstrap.client.js", mode: "client" }],
   modules: ["@nuxt/icon"],
 });
