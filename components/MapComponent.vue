93e809e7451b8a2508602d6bc40d4cdd43f2942b
Siem van Breda <sgw.vanbreda@student.avans.nl>
2025-01-13 15:46:43 +0100

    basic floormap component and page

---
+++ components/MapComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{ rooms: string[] }>();
+
+const emit = defineEmits<{
+  (e: "roomClick", room: string): void;
+}>();
+</script>
+
+<template>
+  <div class="row h-100 pt-3">
+    <template v-for="(room, index) in props.rooms">
+      <div class="col-4 border d-flex align-items-center border-black" :class="{ 'justify-content-end': index % 2 == 0 }" @click="emit('roomClick', room)">{{ room }}</div>
+
+      <div v-if="index % 2 == 0 && rooms.length >= index + 2" class="col-4" :class="{ 'border-top border-black': rooms.length < index + 3 }"></div>
+    </template>
+  </div>
+</template>
+
+<style></style>
---
+++ pages/floormap.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const logClick = (room: string) => {
+  console.log("Clicked room: " + room);
+};
+
+const rooms = ["1", "2", "3", "4", "5", "6", "7", "8", "neo-1", "neo-2"];
+</script>
+
+<template>
+  <div class="container mt-5 pt-2">
+    <div class="mx-auto responsive-width" style="height: 70vh; width: 90vw">
+      <MapComponent @room-click="logClick" :rooms />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.responsive-width {
+  max-width: 75%;
+}
+
+@media only screen and (max-width: 768px) {
+  .responsive-width {
+    max-width: 100%;
+  }
+}
+</style>
