343084df3d4782580b56ce5496296bdb3a0d2f3f
EDivkovic <ed.divkovic@student.avans.nl>
2025-01-13 15:36:39 +0100

    Fixed back-button

--- pages/patients/[id]/addMedicine.vue
+++ pages/patients/[id]/addMedicine.vue
@@ -72,53 +72,56 @@ import { toTypedSchema } from "@vee-validate/zod";
 
 <template>
     <div class="d-md-flex flex-column vh-100 justify-content-center align-items-center mt-5">
-        <div class="w-100 text-start mb-3 ms-4">
-            <button class="btn btn-secondary" @click="goBack">Back</button>
-        </div>
-        <form @submit="onSubmit" class="medicine-form col-sm-12 col-md-8 pe-0 ms-4 me-4 bg-light shadow rounded pt-1 pb-3">
-            <h4 class="text-center mt-2">Voeg medicijn toe</h4>
-            <div class="ms-4 me-4">
-                <div class="mt-2">
-                    <label for="name">Medicijn</label>
-                    <select class="form-select" id="name" v-model="name" @change="updateSelectedMedicineLink">
-                        <option disabled value="">Selecteer een medicijn</option>
-                        <option 
-                            v-for="medicine in medicines" 
-                            :key="medicine.name" 
-                            :value="medicine.name"
-                            :data-link="medicine.formularium_url"
-                        >
-                            {{ medicine.name }}
-                        </option>
-                    </select>
-                    <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
-                </div>
-                <div>
-                    <p v-if="selectedMedicineLink" class="mt-1">
-                        <strong>Link kinderformularium:</strong><br>
-                        <a :href="selectedMedicineLink" target="_blank" class="text-primary">
-                            {{ selectedMedicineLink }}
-                        </a>
-                    </p>
-                </div>
-                <div class="mt-2">
-                    <label for="dosage">Dosering</label>
-                    <input class="form-control input-lg border border-secondary" id="dosage" type="text" v-model="dosage" placeholder="Dosering" />
-                    <div v-if="errors.dosage" class="invalid-feedback">{{ errors.dosage }}</div>
-                </div>
-                <div class="mt-2">
-                    <label for="recurrance">Herhaling</label>
-                    <textarea class="col-12" rows="3" id="recurrance" v-model="recurrance"></textarea>
-                    <div v-if="errors.recurrance" class="invalid-feedback">{{ errors.recurrance }}</div>
-                </div>
-                <div>
-                    <label for="notes">Aantekeningen</label>
-                    <textarea class="col-12" rows="5" id="notes" v-model="notes"></textarea>
-                    <div v-if="errors.notes" class="invalid-feedback">{{ errors.notes }}</div>
-                </div>
-                <button class="medicine-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Voeg medicijn toe</button>
+        <div class="col-sm-12 col-md-8 pe-0 ms-4 me-4">
+            <div class="text-start mb-3">
+                <button class="btn btn-secondary" @click="goBack">Back</button>
             </div>
-        </form>
+
+            <form @submit="onSubmit" class="medicine-form bg-light shadow rounded pt-1 pb-3">
+                <h4 class="text-center mt-2">Voeg medicijn toe</h4>
+                <div class="ms-4 me-4">
+                    <div class="mt-2">
+                        <label for="name">Medicijn</label>
+                        <select class="form-select" id="name" v-model="name" @change="updateSelectedMedicineLink">
+                            <option disabled value="">Selecteer een medicijn</option>
+                            <option 
+                                v-for="medicine in medicines" 
+                                :key="medicine.name" 
+                                :value="medicine.name"
+                                :data-link="medicine.formularium_url"
+                            >
+                                {{ medicine.name }}
+                            </option>
+                        </select>
+                        <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
+                    </div>
+                    <div>
+                        <p v-if="selectedMedicineLink" class="mt-1">
+                            <strong>Link kinderformularium:</strong><br>
+                            <a :href="selectedMedicineLink" target="_blank" class="text-primary">
+                                {{ selectedMedicineLink }}
+                            </a>
+                        </p>
+                    </div>
+                    <div class="mt-2">
+                        <label for="dosage">Dosering</label>
+                        <input class="form-control input-lg border border-secondary" id="dosage" type="text" v-model="dosage" placeholder="Dosering" />
+                        <div v-if="errors.dosage" class="invalid-feedback">{{ errors.dosage }}</div>
+                    </div>
+                    <div class="mt-2">
+                        <label for="recurrance">Herhaling</label>
+                        <textarea class="form-control" rows="3" id="recurrance" v-model="recurrance"></textarea>
+                        <div v-if="errors.recurrance" class="invalid-feedback">{{ errors.recurrance }}</div>
+                    </div>
+                    <div>
+                        <label for="notes">Aantekeningen</label>
+                        <textarea class="form-control" rows="5" id="notes" v-model="notes"></textarea>
+                        <div v-if="errors.notes" class="invalid-feedback">{{ errors.notes }}</div>
+                    </div>
+                    <button class="medicine-button btn btn-secondary btn-lg mt-3 col-12" type="submit">Voeg medicijn toe</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
